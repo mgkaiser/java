711ae8462b5118ff0fa24d8ce5389d3b2f5ad423
Michael Kaiser <mkaiser@incomm.com>
2019-10-05 00:45:00 -0400

    Added comments to Ch14Ex06

--- Ch14Ex06/JSandwich.java
+++ Ch14Ex06/JSandwich.java
@@ -22,43 +22,50 @@ public class JSandwich extends JFrame
 		
 		// Set the layout
 		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-		
-		// Add stuff here		
-		
+				
+		// Bread Panel
 		JPanel breadPanel = new JPanel();
 		breadPanel.setLayout(new BoxLayout(breadPanel, BoxLayout.Y_AXIS));				
 		breadPanel.add(new Label("Bread"));	
 		breadPanel.add(new Label("Test2"));
-				
+		
+		// Meat Panel
 		JPanel meatPanel = new JPanel();
 		meatPanel.setLayout(new BoxLayout(meatPanel, BoxLayout.Y_AXIS));				
 		meatPanel.add(new Label("Meat"));	
 		meatPanel.add(new Label("Test2"));
 				
+		// Cheese Panel
 		JPanel cheesePanel = new JPanel();		
 		cheesePanel.setLayout(new BoxLayout(cheesePanel, BoxLayout.Y_AXIS));				
 		cheesePanel.add(new Label("Cheese"));	
 		cheesePanel.add(new Label("Test2"));
 		
+		// Make Sandwich Button
 		JButton makeSandwichButton = new JButton("Make My Sandwich");
 		makeSandwichButton.setFont(_buttonFont);
 				
+		// Sandwich Amount Prompt
 		JLabel sandwichAmountPromptLabel = new JLabel("Your sandwich costs: ");
 		sandwichAmountPromptLabel.setFont(_labelFont);
 		
+		// Sandwich Amount
 		JLabel sandiwchAmountLabel = new JLabel("Test");
 		sandiwchAmountLabel.setFont(_labelFont);
 		
+		// Sandwich choices panel.  Contains bread, meat, and cheese panel
 		JPanel sandwichChoicesPanel = new JPanel();		
 		sandwichChoicesPanel.add(breadPanel);
 		sandwichChoicesPanel.add(meatPanel);
 		sandwichChoicesPanel.add(cheesePanel);
 		add(sandwichChoicesPanel);				
 							
+		// Button panel.  Contains the "Make Sandwich" button
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(makeSandwichButton);
 		add(buttonPanel);			
 		
+		// Sandwich Amount panel.  Contains the amount prompt and amount labels.
 		JPanel sandwichAmountPanel = new JPanel();		
 		sandwichAmountPanel.add(sandwichAmountPromptLabel);
 		sandwichAmountPanel.add(sandiwchAmountLabel);
